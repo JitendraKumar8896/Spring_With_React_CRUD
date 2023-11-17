5e5b1ad2d8ee96fb68ab514a634b8a4ce9046fe5
JitendraKumar8896 <jk8752277@gmail.com>
2023-11-17 11:37:25 +0530

    first commit

---
+++ project/SpringFullStack/src/main/java/com/spring/SpringFullStack/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.spring.SpringFullStack.repository;
+
+import com.spring.SpringFullStack.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+
+
+
+
+}
